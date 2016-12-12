ecb29769609c687f063813d989ea0a21d5844c7d
Sam <sam.saffron@gmail.com>
2016-12-12 15:55:28 +1100

    initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+## discourse-unhandled-tagger
+
+This simple plugin adds the tag "unhandled" to every topic if there is a non-staff reply.
+
---
+++ plugin.rb
@@ -0,0 +1,24 @@
+# name: discourse-unhandeled-tagger
+# about: Add an "unahndeled" tag to every topic where non-staff post
+# version: 0.1
+# authors: Sam Saffron
+
+PLUGIN_NAME = "discourse_unhandeled-tagger".freeze
+
+after_initialize do
+
+  DiscourseEvent.on(:post_created) do |post, _, user|
+    topic = post.topic
+    unless user.staff? || topic.private_message?
+      tag = Tag.find_by(name: "unhandled")
+      unless tag
+        tag = Tag.create!(name: "unhandled")
+      end
+      topic.tags ||= []
+      topic.tags << tag
+      topic.save
+
+      post.publish_change_to_clients!(:revised, reload_topic: true)
+    end
+  end
+end

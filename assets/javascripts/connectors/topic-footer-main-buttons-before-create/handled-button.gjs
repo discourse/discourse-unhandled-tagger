c4585feeee1153ca3a0aeb26b3a2090457e4dfaf
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-11-22 13:21:50 +0100

    FIX: makes handled button reactive to change (#27)
    This commit is ensuring that the state of the button correctly changes according to the presence of the unhandled tag on the topic.
    
    The main issue was the lack of `.get` to ensure we track changes to the tags array.
    
    This commit also:
    - uses `gjs` for the handled-button component
    - modernises the component code
    - adds a full system spec for the plugin
    - writes a page object to use for this plugin

---
+++ assets/javascripts/connectors/topic-footer-main-buttons-before-create/handled-button.gjs
@@ -0,0 +1,65 @@
+import Component from "@glimmer/component";
+import { fn } from "@ember/helper";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
+import DButton from "discourse/components/d-button";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import Topic from "discourse/models/topic";
+
+export default class HandledButton extends Component {
+  static shouldRender({ topic }, { siteSettings, currentUser }) {
+    return (
+      currentUser?.staff &&
+      !topic.isPrivateMessage &&
+      siteSettings.tagging_enabled &&
+      siteSettings.unhandled_tag
+    );
+  }
+
+  @service appEvents;
+  @service siteSettings;
+
+  get topic() {
+    return this.args.outletArgs.topic;
+  }
+
+  get handled() {
+    return !this.topic.get("tags")?.includes?.(this.siteSettings.unhandled_tag);
+  }
+
+  @action
+  async setUnhandled(value) {
+    const tags = this.topic.tags;
+    tags.removeObject(this.siteSettings.unhandled_tag);
+    if (value) {
+      tags.addObject(this.siteSettings.unhandled_tag);
+    }
+
+    try {
+      await Topic.update(this.topic, { tags });
+      this.appEvents.trigger("header:show-topic", this.topic);
+    } catch (e) {
+      popupAjaxError(e);
+    }
+  }
+
+  <template>
+    {{#if this.handled}}
+      <DButton
+        class="unhandle"
+        @icon="times-circle"
+        @action={{fn this.setUnhandled true}}
+        @label="unhandled_tagger.unhandle.title"
+        @title="unhandled_tagger.unhandle.help"
+      />
+    {{else}}
+      <DButton
+        class="handle"
+        @icon="check-circle"
+        @action={{fn this.setUnhandled false}}
+        @label="unhandled_tagger.handled.title"
+        @title="unhandled_tagger.handled.help"
+      />
+    {{/if}}
+  </template>
+}
---
+++ spec/system/smoke_test_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+describe "DiscourseUnhandledTagger | smoke test", type: :system do
+  fab!(:current_user) { Fabricate(:admin) }
+  fab!(:tag)
+  fab!(:topic) { Fabricate(:topic, tags: [tag]) }
+  fab!(:post) { create_post(topic: topic, raw: "this is a post which should be handled") }
+
+  let(:topic_page) { PageObjects::Pages::Topic.new }
+  let(:unhandled_tagger_page) { PageObjects::Pages::UnhandledTagger.new }
+
+  before { SiteSetting.tagging_enabled = true }
+
+  context "when anonymous" do
+    it "doesn’t show the button" do
+      topic_page.visit_topic(post.topic)
+
+      expect(unhandled_tagger_page).to be_disabled
+    end
+  end
+
+  context "when logged in" do
+    before { sign_in(current_user) }
+
+    it "can toggle handling" do
+      topic_page.visit_topic(post.topic)
+
+      expect(unhandled_tagger_page).to be_unhandled
+
+      unhandled_tagger_page.handle
+
+      expect(unhandled_tagger_page).to be_handled
+
+      unhandled_tagger_page.unhandle
+
+      expect(unhandled_tagger_page).to be_unhandled
+    end
+
+    context "when tagging is disabled" do
+      before { SiteSetting.tagging_enabled = false }
+
+      it "doesn’t show the button" do
+        topic_page.visit_topic(post.topic)
+
+        expect(unhandled_tagger_page).to be_disabled
+      end
+    end
+
+    context "when unhandled tag is undefined" do
+      before { SiteSetting.unhandled_tag = nil }
+
+      it "doesn’t show the button" do
+        topic_page.visit_topic(post.topic)
+
+        expect(unhandled_tagger_page).to be_disabled
+      end
+    end
+
+    context "when current user is not staff" do
+      fab!(:current_user) { Fabricate(:user) }
+
+      it "doesn’t show the button" do
+        topic_page.visit_topic(post.topic)
+
+        expect(unhandled_tagger_page).to be_disabled
+      end
+    end
+  end
+end
